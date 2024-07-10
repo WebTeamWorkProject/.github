783bc9ab443433d8e3e187e58666c457d94e1485
Ivo Dimitrov <lautaspartan@gmail.com>
2024-07-10 14:16:12 +0300

    constants

---
+++ main/src/src/constants/constat.js
@@ -0,0 +1,13 @@
+export const HOME = 'home';
+
+export const FAVORITES = 'favorites';
+
+export const ABOUT = 'about';
+
+export const CONTAINER_SELECTOR = '#container';
+
+export const FULL_HEART = '❤';
+
+export const EMPTY_HEART = '♡';
+
+export const apiKey = '38DPn6zPeItmyenCp0D5vSiNMfzDa1NZ';
